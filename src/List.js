011fe9c992592b1599217222b7e24d43b7b57ad0
Lucas Coelho <l.coelhooliveira@gmail.com>
2019-02-07 17:59:27 -0200

    Add space image delete

--- src/List.js
+++ src/List.js
@@ -4,24 +4,35 @@ import {Col, Row,} from 'reactstrap'
 
 function List(props){
   const bookmarksList = props.bookmarks.map((bookmarks,i) =>   
-  <div className='singleBookmarks' key={i} id={i}>
-    <Row>
-      <Col xs='10'>
-          <h3>{bookmarks.title}</h3>
-          <p>
-            <a href={bookmarks.link} target="_blank" rel="noopener noreferrer">{bookmarks.link}</a>
-          </p>
-          <ul className='Tags'>
-            {
-              bookmarks.tags.map((val, index) => <li data={val} indextag={[index]} key={index}>{val}<span className='remove-icon' onClick={() => props.removeTag(i + ' ' + index) } ></span></li> )
-            }
-          </ul>
-      </Col>
-      <Col xs='2' id='deleteColumn'>
-        <p className="deleteBookmarks" id={bookmarks.id} onClick={() => props.removeBookmarks(i)}><img src={require('./static/images/Trash.svg')} alt='Add' />DELETE</p>
-      </Col>
-    </Row>
-    </div>
+		<div className='singleBookmarks' key={i} id={i}>
+			<Row>
+				<Col xs='10'>
+					<h3>{bookmarks.title}</h3>
+					<p>
+						<a href={bookmarks.link} target="_blank" rel="noopener noreferrer">
+							{bookmarks.link}
+						</a>
+					</p>
+					<ul className='Tags'>
+						{
+							bookmarks.tags.map((val, index) => 
+								<li data={val} indextag={[index]} key={index}>
+									{val}
+									<span className='remove-icon' onClick={() => props.removeTag(i + ' ' + index) } >
+									</span>
+								</li> 
+							)
+						}
+					</ul>
+				</Col>
+				<Col xs='2' id='deleteColumn'>
+					<p className="deleteBookmarks" id={bookmarks.id} onClick={() => props.removeBookmarks(i)}>
+						<img src={require('./static/images/Trash.svg')} alt='Add' />
+						DELETE
+					</p>
+				</Col>
+			</Row>
+		</div>
   );
   return (
     <div className='Bookmarks'>
